8fa87a6e36c07e74e959b18ead5b86fc09f9b61e
DMarques Coleman <dmcolem2@asu.edu>
2022-11-18 08:56:00 -0600

    DCentric logo added

--- app.js
+++ app.js
@@ -1,3 +1,53 @@
+/* SPLASH SCREEN */
+
+
+let intro = document.querySelector(".intro");
+let logo = document.querySelector(".logo-header");
+let logoSpan = document.querySelectorAll(".logo");
+
+
+
+ window.addEventListener("DOMContentLoaded", ()=> {
+	
+ 	setTimeout(()=> {
+		
+		logoSpan.forEach((span, idx)=> {
+			
+			setTimeout(()=> {
+ 				span.classList.add("active");
+ 			}, (idx + 1) * 400)
+			
+ 		});
+		
+ 		setTimeout(()=> {
+ 			logoSpan.forEach((span, idx)=> {
+				
+ 				setTimeout(()=> {
+ 					span.classList.remove("active");
+ 					span.classList.add("fade");
+ 				}, (idx + 1) * 50);
+ 			})
+ 		},2000);
+		
+		
+ 		setTimeout(()=> {
+ 			intro.style.top = "-100vh";
+ 		}, 2300)
+		
+		
+ 	})
+ })
+
+ /* END */
+
+
+
+
+
+
+
+
+
 const sections = document.querySelectorAll('.section');
 const sectBtns = document.querySelectorAll('.controlls');
 const sectBtn = document.querySelectorAll('.control');
